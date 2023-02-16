d205f1148e764d8adc82a3393bfca59e4a0d2d8a
Rahul Manavalan <rahul.learns.physics@gmail.com>
2023-02-16 15:36:42 +0000

    initial push

---
+++ Incompressible_NS/datagen.jl
@@ -0,0 +1,19 @@
+using WaterLily
+using LinearAlgebra : norm2
+using Distributions
+
+function spline()
+
+end 
+
+function random_shape()
+
+end 
+
+function plot_shape()
+
+end 
+
+function generate_shape()
+
+end 
